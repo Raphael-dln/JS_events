068b4f401e9bfd546515ae985422158cb84dfb6a
Raphael-dln <delannay.raphael@gmail.com>
2020-05-25 18:32:38 +0200

    ex 8

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// On commence par un petit échauffement : lorsque l'utilisateur va cliquer sur le footer (portant le tag <footer>), tu vas afficher le mot "clique" en console.
-// Maintenant on va upgrader cette première fonctionnalité : lorsque l'utilisateur va cliquer sur le footer, tu vas afficher en console "clic numéro x" avec x qui commence à 1 et s'incrémente de +1 à chaque clic.
+// Fonctionnalité 1
+// Fonctionnalité 1 bis
 
 let footer = document.getElementsByTagName('footer')[0];
 
@@ -12,7 +12,6 @@ let onFooterClick = function() {
 footer.addEventListener('click', onFooterClick);
 
 
-// On va enfin faire fonctionner ce satané "Hamburger Menu" qui s'affiche depuis le début mais qui n'actionne rien quand on clique dessus
 
 // Fonctionnalité 2 : toggle burger
 let navElmt = document.getElementById('navbarHeader');
@@ -24,7 +23,7 @@ let onNavbarClick = function() {
 navTogglerElmt.addEventListener('click', onNavbarClick);
 
 
-// À présent, on va faire cela : si on clique sur le bouton "Edit" de la première card, le texte de la card va se mettre en rouge de façon irréversible (sauf si on recharge la page). À toi de jouer !
+// Fonctionnalité 3
 
 let btnEdit = document.getElementsByClassName('btn-sm btn-outline-secondary')[0];
 let cardText = document.getElementsByClassName('card-text')[0];
@@ -34,7 +33,7 @@ let onBtnEditClick = function() {
 btnEdit.addEventListener('click', onBtnEditClick);
 
 
-//On va faire quelque chose de similaire à la fonctionnalité 3 mais un peu plus complexe : si on clique sur le bouton "Edit" de la deuxième card, le texte de la card va se mettre en vert. Si on re-clique dessus, il redevient comme avant ! Tu l'as compris, il va falloir que tu cherches comment faire un "toggle" sur le style du texte. C'est plus compliqué que sur une classe.
+// Fonctionnalité 4
 
 let btnEdit2 = document.getElementsByClassName('btn-sm btn-outline-secondary')[1];
 let cardText2 = document.getElementsByClassName('card-text')[1];
@@ -52,7 +51,7 @@ let onBtnEditClick3 = function() {
 btnEdit2.addEventListener('click', onBtnEditClick2);
 
 
-// Pour le fun, on va implémenter une fonctionnalité à la sauce ☢"nucléaire"🤯. Et comme elle est un peu dangereuse, on va la cacher…
+// Fonctionnalité 5
 
 let bootstrap = document.getElementsByTagName('header')[0];
 let link = document.getElementsByTagName('link')[0];
@@ -71,8 +70,7 @@ let onHeaderClick2 = function() {
 bootstrap.addEventListener('dblclick', onHeaderClick); 
 
 
-//T'as déjà implémenté 5 fonctionnalités d'interaction ! C'est top ! On va commencer à corser les choses.
-//La fonctionnalité sera la suivante : si un utilisateur passe sa souris sur le bouton "View" d'une card (n'importe laquelle), celle-ci va se réduire. Cela veut dire que le texte disparaît, l'image n'apparaîtra qu'à 20 % de sa taille d'origine et les boutons "Edit" / "View" restent visibles. Cette fonction sera réversible : s'il repasse sa souris, la card redevient normale !
+// Fonctionnalité 6
 
 let btnView = document.getElementsByClassName('btn btn-sm btn-success');
 let cardText4 = document.getElementsByClassName('card-text');
@@ -90,7 +88,24 @@ let cardImg = document.getElementsByClassName("card-img-top");
     })
  }
 
+// Fonctionnalité 7
+
+let cardElt = document.querySelector('.album > .container > .row');
+let btnNextElt = document.querySelector('.btn.btn-secondary.my-2');
+
+btnNextElt.addEventListener('click', function()  {
+  cardElt.insertBefore(cardElt.lastElementChild, cardElt.firstElementChild);
+})
 
 
+// Fonctionnalité 8 
 
+let btnPreviousElt = document.getElementsByClassName('btn btn-primary my-2')[0];
+
+btnPreviousElt.removeAttribute('href');
+btnPreviousElt.addEventListener('click', function (){
+  cardElt.insertBefore(cardElt.firstElementChild, cardElt.lastElementChild.nextSibling);
+})
+
+// Fonctionnalité 9 
 
